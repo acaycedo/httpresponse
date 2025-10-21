846e49fd71d14cb3829aa3bd589253c05e256e5f
acaycedo <afcaycedo14@gmail.com>
2025-10-21 16:59:20 -0500

    Creacion de HttpResponse

---
+++ src/test/java/com/devsenior/acaycedo/httpresponse/HttpresponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.acaycedo.httpresponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpresponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
